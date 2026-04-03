3e3f8c1bdfa2c4a67f4ad2c41913dc6b257276a1
Eleonore <galiacye@gmail.com>
2026-04-03 16:48:17 +0200

    header ds layout + css

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -11,31 +11,35 @@
 
 <body>
 
-   <header class="bg-dark text-light py-3">
+   <header class="bg-dark text-light py-3"> <!-- py = top+bottom -->
     <div class="ban ">
         <img src="<?= base_url('./img/eddy-bd.jpeg') ?>" class="eddy">
         <div class="container text-center">
-            <h1 class="hero-title">Chef Eddy</h1>
+            <div class="ligne"><h1 class="hero-title">Chef Eddy  <span class="fusil">o</span></h1></div>
             <h2 class="txt">On ne rigole pas avec les grammages...</h2>
         </div>
         <img src="<?= base_url('./img/logo.png') ?>" alt="logo" class="logo">
 
     </div>
 </header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">  <!-- expand-lg =>se replie en deçà de 1024 -->
+   
     <div class="container">
-        <a class="navbar-brand" href="<?= base_url('users') ?>">Utilisateurs</a>
+        
         <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="<?= base_url('users') ?>">Utilisateurs</a>
+            </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?= base_url('recipeIndex') ?>">Toutes les recettes</a>
             </li>
-            <li>
+            <li class="nav-item">
                 <a class="nav-link" href="<?= base_url('categories') ?>"> Les Catégories</a>
             </li>
-            <li>
+            <li class="nav-item">
                 <a class="nav-link" href="<?= base_url('tags') ?>">Tags</a>
             </li>
-            <li>
+            <li class="nav-item">
                 <a class="nav-link" href="<?= base_url('ingredients') ?>">Recherche par ingrédients</a>
             </li>
         </ul>
