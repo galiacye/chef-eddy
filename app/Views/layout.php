db48297434cc2ee7f215a4121cdf6635c032a591
Eleonore <galiacye@gmail.com>
2026-05-18 15:33:41 +0200

    front stable

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
-    <!-- Ton CSS -->
+  
     <link href="<?= base_url('./css/layout.css') ?>" rel="stylesheet">
 
     <?= $this->renderSection('custom-css') ?>
