55d53e2d080aadf1f6a124dcca2b4917814f1ff8
Leo <galiacye@gmail.com>
2026-05-23 18:56:25 +0200

    wip

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -33,7 +33,7 @@ class Home extends BaseController
             'categories' => $ingredientsCategories,
             'tags' => $tags
         ];
-        return view('Home/indexV2', $data);
+        return view('Home/index', $data);
     }
 
     public function salut()
