5d5d974a9c0539bef30a6423282fdc84b0057d8b
galiacye <galiacye@gmail.com>
2026-05-31 14:50:39 +0200

    layoutAdmin.css

--- app/Views/layoutAdmin.php
+++ app/Views/layoutAdmin.php
@@ -1,87 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?= $this->renderSection('title') ?>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-
-    <link href="<?= base_url('./css/header-mq.css') ?>" rel="stylesheet">
+    <title>Admin - <?= $this->renderSection('titre') ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url('css/Admin/layoutAdmin.css') ?>" rel="stylesheet">
     <?= $this->renderSection('custom-css') ?>
 </head>
 
-<body>
+<body class="bg-light">
 
-    <header class="bg-dark text-light py-3">
-        <!-- py = top+bottom -->
-        <div class="ban ">
-            <img src="<?= base_url('./img/eddy-bd.jpeg') ?>" class="eddy">
-            <div class="container text-center">
-                <h1 class="hero-title">Chef Eddy </h1>
-                <h2 class="text-light">On ne rigole pas avec les grammages...</h2>
+    <nav class="navbar navbar-dark bg-dark navbar-expand-md"><!--expand: déplie le burger à partir de md-->
+        <div class="container-fluid">
+
+            <a class="navbar-brand" href="/Admin">
+                <img src="<?= base_url('img/eddy-bd.jpeg') ?>" height="80" alt="Chef Eddy" class="eddy">
+            </a>
+            <button class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#mainNavbar"
+                aria-controls="mainNavbar"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+
+                <span class="navbar-toggler-icon"></span>
+
+            </button>
+            <div class="collapse navbar-collapse" id="mainNavbar">
+                <span class="navbar-brand"> Admin Chef Eddy</span>
+                <div>
+                    <a href="/dashboard" class="btn btn-outline-light btn-sm me-2">Tableau de bord</a>
+                    <a href="/Admin/recipes-index" class="btn btn-outline-light btn-sm me-2">Recettes</a>
+                    <a href="/Admin/users-index" class="btn btn-outline-light btn-sm me-2">Utilisateurs</a>
+                    <a href="/" class="btn btn-outline-warning btn-sm">Site</a>
+                </div>
             </div>
-            <img src="<?= base_url('./img/logo.png') ?>" alt="logo" class="logo">
-
         </div>
-    </header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4"> <!-- expand-lg =>se replie en deçà de 1024 -->
-
-        <div class="container d-flex justify-content-between">
-            <div class="left">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= base_url('index.php') ?>">Accueil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= base_url('recipe-index') ?>">Toutes les recettes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= base_url('category/index') ?>"> Les Catégories</a>
-                    </li>
-
-                    <li>
-                        <a class="nav-link" href="<?= base_url('/add-recipe') ?>">Proposez une recette</a>
-                        <!--enverra sur s'inscrire-->
-                    </li>
-                </ul>
-            </div>
-            <div class="right">
-                <ul class="navbar-nav">
-                    <?php if (session()->has('user_id')): ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?= base_url('profile') ?>">Mon Profil</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?= base_url('mes-recettes') ?>">Mes Recettes</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?= base_url('logout') ?>">Se Déconnecter</a>
-                        </li>
-                </ul>
-            <?php else: ?>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= base_url('Admin/login') ?>">Déjà membre</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= base_url('User/register') ?>">S'inscrire</a>
-                    </li>
-                </ul>
-            <?php endif; ?>
-            </div>
     </nav>
 
-    <main class="container-fluid px-0 w-100">
+    <main class="container-fluid px-0">
         <?= $this->renderSection('body') ?>
+
     </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <?= $this->renderSection('customJs') ?>
-
-
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <?= $this->renderSection('customjs') ?>
 </body>
 
 </html>
