b53284a2edc48d93701085c5321d5737336d46d1
galiacye <galiacye@gmail.com>
2026-06-20 19:33:38 +0200

    reparation reply-editor en cours

--- app/Views/layoutAdmin.php
+++ app/Views/layoutAdmin.php
@@ -47,7 +47,7 @@
 
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <?= $this->renderSection('customjs') ?>
+    <?= $this->renderSection('customJs') ?>
 </body>
 
 </html>
