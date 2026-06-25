4eceb84c175225f5cfe533ecc0499714a1393766
galiacye <galiacye@gmail.com>
2026-06-25 13:11:23 +0200

    css world index

--- app/Views/world/worldIndex.php
+++ app/Views/world/worldIndex.php
@@ -5,24 +5,26 @@
 <?= $this->endSection() ?>
 
 <?= $this->section('custom-css') ?>
-<link href="<?= base_url('css/recipes/recipe.css') ?>" rel="stylesheet">
+<link href="<?= base_url('css/tags/world-tag.css') ?>" rel="stylesheet">
 <?= $this->endSection() ?>
 
 <?= $this->section('body') ?>
 <div class="container">
 
-    <h1>Cuisine du monde</h1>
+    <h1 class="text-center mt-4 mb- title">Cuisine du monde</h1>
+    <!-- liste des pays retournés par l'api, affichés comme des tags cliquables  -->
+    <div class="row">
+        <div class="btn-bloc d-flex gap-2 flex-wrap justify-content-between">
+            <?php foreach ($countries as $country): ?>
 
-     <!-- liste des pays retournés par l'api, affichés comme des tags cliquables  -->
-    <div class="tags">
-       
-        <?php foreach ($countries as $country): ?>
-            <a href="<?= site_url('cuisine-du-monde/' . urlencode($country['strArea'])) ?>" class="tag">
-                <!--strArea: clé json du pays-->
-                <?= esc($country['strArea']) ?>
-            </a>
-        <?php endforeach ?>
+                <a href="<?= site_url('cuisine-du-monde/' . urlencode($country['strArea'])) ?>" class="btn my-1 btn-worldTag">
+                    <!--strArea: clé json du pays-->
+                    <?= esc($country['strArea']) ?>
+                </a>
+            <?php endforeach ?>
+        </div>
     </div>
+
 
 </div>
 <?= $this->endSection() ?>
